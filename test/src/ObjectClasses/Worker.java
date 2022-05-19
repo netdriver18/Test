59236bc3bcc39288742836dbf80ff14d82f2e1e9
Natalie Tkach <netDriver18@gmail.com>
2022-05-19 22:27:40 +0300

    Object yome task 1

--- test/src/ObjectClasses/Worker.java
+++ test/src/ObjectClasses/Worker.java
@@ -1,2 +1,21 @@
-package ObjectClasses;public class Worker {
+package ObjectClasses;
+public class Worker {
+   private int age;
+   private String name;
+  // private String position;
+
+    public void setAge(int age){
+        this.age = age;
+    }
+    public int getAge(){
+        return age;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
